54f054e854d5c2de872d016cec4c9a83770a37a9
YonghwaJeong <yonghwa814@naver.com>
2024-01-19 15:33:20 +0900

    [fix] User모델 권한 충돌, Authentication_backends 문제 해결

--- apps/cardgame/models.py
+++ apps/cardgame/models.py
@@ -2,11 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django.conf import settings
 import random
 
 # 유저 Profile 모델
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     score = models.IntegerField(default=0)
 
     def __str__(self):
--- apps/user/models.py
+++ apps/user/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 # Create your models here.
-class User(AbstractUser):
+class CustomUser(AbstractUser):
   profile_image = models.ImageField(
     "프로필 이미지", upload_to="user/profile", blank=True
   )
