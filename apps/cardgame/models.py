c813d5c6629c4b03df77cae5aa5f27629a0c1386
YonghwaJeong <yonghwa814@naver.com>
2024-01-19 17:06:58 +0900

    [fix] Custom user의 Profile 자동 생성 안되는 문제 fix

--- apps/cardgame/models.py
+++ apps/cardgame/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
@@ -43,7 +44,13 @@ class Game(models.Model):
     def __str__(self):
         return f"{self.id} - {self.attacker.user.username} VS {self.defender.user.username}"
 
-@receiver(post_save, sender=User)
+user = get_user_model()
+@receiver(post_save, sender=user)
 def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def create_user_profile_social(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
