f94c2dfdf013055f15b0db14722c6f3d4ae6acb2
Khin Wai Yan <khinwaiyan@snu.ac.kr>
2024-01-19 18:41:14 +0900

    [fix] fix urls in user apps

--- apps/user/urls.py
+++ apps/user/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import *
+from .views import login_view, logout_view, signup
 
 urlpatterns = [
-  path("login/", login_view),
-  path('logout/', logout_view),
-  path('signup/', signup),
+    path('login/', login_view, name='login_view'),
+    path('logout/', logout_view, name='logout_view'),
+    path('signup/', signup, name='signup_view'),
 ]
